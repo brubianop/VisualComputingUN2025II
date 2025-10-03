0e89e50291ab5943b236dcadde7e66425f422783
brubianop <130870902+brubianop@users.noreply.github.com>
2025-10-03 13:44:33 -0500

    Add scene objects with PBR textures. Animation, Shadows, Reflections.

--- 2025-10-03_Taller_1_Materiales_Iluminacion_Color/threejs/t1matilcol/src/App.jsx
+++ 2025-10-03_Taller_1_Materiales_Iluminacion_Color/threejs/t1matilcol/src/App.jsx
@@ -1,31 +1,37 @@
 import { Suspense, useCallback, useRef, useState, useEffect, useLayoutEffect } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls, useAnimations, useGLTF, useTexture } from '@react-three/drei';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, Environment, useAnimations, useGLTF, useTexture, CubeCamera, RenderTexture } from '@react-three/drei';
 import * as THREE from 'three';
 
 //-------------------------------- PRESETS ---------------------------------------//
 
+// POSITIONS - ANIMATIONS REF
+const CUBE_POSITION = new THREE.Vector3(160, 10, 112);
+
+const SPHERE_RADIUS = 10;
+const ORBIT_RADIUS = 25; // Distance from the Cube center to the Sphere
+const ROTATION_SPEED = 0.005; 
 //TEXTURES
 
 const GROUND_TEXTURE = {
   map: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_Color.jpg",   
-  normal: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_NormalGL.jpg", 
-  roughness: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_Roughness.jpg",
-  ao: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_AmbientOcclusion.jpg", 
+  normalMap: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_NormalGL.jpg", 
+  roughnessMap: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_Roughness.jpg",
+  aoMap: "/textures/Bricks097_1K-JPG/Bricks097_1K-JPG_AmbientOcclusion.jpg", 
 }; 
 
 const CUBE_TEXTURE = {
   map: "/textures/PavingStones138_1K-JPG/PavingStones138_1K-JPG_Color.jpg",   
-  normal: "/textures/PavingStones138_1K-JPG/PavingStones138_1K-JPG_NormalGL.jpg", 
-  roughness: "/textures/Bricks097_1K-JPG/PavingStones138_1K-JPG_Roughness.jpg",
-  ao: "/textures/Bricks097_1K-JPG/PavingStones138_1K-JPG_AmbientOcclusion.jpg",
+  normalMap: "/textures/PavingStones138_1K-JPG/PavingStones138_1K-JPG_NormalGL.jpg", 
+  roughnessMap: "/textures/PavingStones138_1K-JPG/PavingStones138_1K-JPG_Roughness.jpg",
+  aoMap: "/textures/PavingStones138_1K-JPG/PavingStones138_1K-JPG_AmbientOcclusion.jpg",
 };
 
 const SPHERE_TEXTURE = {
   map: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_Color.jpg",   
-  normal: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_NormalGL.jpg", 
-  roughness: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_Roughness.jpg",
-  metalness: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_Metalness.jpg",
+  normalMap: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_NormalGL.jpg", 
+  roughnessMap: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_Roughness.jpg",
+  metalnessMap: "/textures/Metal049A_1K-JPG/Metal049A_1K-JPG_Metalness.jpg",
 };
 
 //CAM CONFIG
@@ -39,8 +45,8 @@ const SCENE_CAMS = [
 
     { 
         name: "Orthographic", 
-        position: [0, 20, -50],
-        zoom: 5,
+        position: [0, 80, 0],
+        zoom: 2,
     },
 
 ];
@@ -55,7 +61,7 @@ const propObjPath = "/glb_models/combat_robot.glb";
 //Environment Object
 const envObjPath = "/glb_models/ussr_launch_site.glb";
 
-
+//--------------------------------- OWN OBJECTS --------------------------------------------------------------//
 function Ground () {
 
   const meshRef = useRef();
@@ -103,6 +109,110 @@ function Ground () {
   );
 }
 
+
+function Sphere () {
+
+  const meshRef = useRef();
+  const reflectCamRef = useRef();
+  const textures = useTexture(SPHERE_TEXTURE);
+
+  const angle = useRef(0);
+
+  const initialPos = [
+    CUBE_POSITION.x + ORBIT_RADIUS, 
+    CUBE_POSITION.y, 
+    CUBE_POSITION.z
+  ];
+
+  // 💡 Animation
+  useFrame((state, delta) => {
+
+    const angleChange = delta * ROTATION_SPEED * 100;
+    angle.current += angleChange;
+
+    const x = CUBE_POSITION.x + Math.cos(angle.current) * ORBIT_RADIUS;
+    const z = CUBE_POSITION.z + Math.sin(angle.current) * ORBIT_RADIUS;
+    
+    const y = CUBE_POSITION.y;
+    
+    if (reflectCamRef.current) {
+      reflectCamRef.current.position.set(x, y, z);
+    }
+        
+    if (meshRef.current) {
+        const distanceTraveled = ORBIT_RADIUS * angleChange;
+        const requiredRollAngle = distanceTraveled / SPHERE_RADIUS;
+
+        meshRef.current.rotation.z += requiredRollAngle;  
+    }
+  });
+
+  return (
+
+    <CubeCamera
+      ref = {reflectCamRef} 
+      resolution = {256} 
+      frames = {5} 
+      near = {1} 
+      far = {1500} 
+      position = {initialPos}
+    >
+
+      {(renderTarget) => (
+        <mesh 
+          ref = {meshRef}
+          position = {[0, 0, 0]}
+          castShadow
+          receiveShadow
+          //reflections
+          onBeforeRender = {({ gl, scene, camera }) => {
+            if (meshRef.current) {
+              meshRef.current.visible = false;
+            }
+          }}
+
+          onAfterRender = {() => {
+            if (meshRef.current) {
+              meshRef.current.visible = true;
+            }
+          }}
+        >
+
+          <sphereGeometry args={[SPHERE_RADIUS, 128, 128]} />
+          <meshStandardMaterial 
+            {...textures}
+            metalness = {1.0}
+            envMap = {renderTarget.texture} 
+          />
+        </mesh>
+      )}
+    </CubeCamera>
+
+  );
+}
+
+function Cube () {
+
+  const meshRef = useRef();
+  const textures = useTexture(CUBE_TEXTURE);
+
+  return (
+    <mesh 
+      position = {[160, 10, 112]}
+      castShadow
+      receiveShadow
+    >
+
+      <boxGeometry args={[20, 20, 20]} />
+      <meshStandardMaterial 
+        {...textures}
+      />
+    </mesh>
+
+  );
+}
+
+// ------------------------------------------ CRAP OBJECTS -----------------------------------------------------------//
 function EnvObject() {
 
   const groupRef = useRef();
@@ -348,6 +458,7 @@ function CamHandler({ cams }) {
 
   const perspectiveCamRef = useRef();
   const orthoCamRef = useRef();
+  const controlRef = useRef();
 
   const curCams = [
     {...cams[0], ref: perspectiveCamRef},
@@ -378,15 +489,32 @@ function CamHandler({ cams }) {
     const nextCam = curCams[activeCamIdx].ref.current;
     if (nextCam && nextCam !== activeSceneCam) {
         set({ camera: nextCam });
-        
-        //Orthographic edge case handling
+
+      //Orthographic HARD reset
+      if (controlRef.current) {
         if (curCams[activeCamIdx].name === "Orthographic") {
-            nextCam.updateProjectionMatrix();
+          controlRef.current.target.set(0, 0, 0); 
+          nextCam.copy(nextCam); 
+        } else { 
+          controlRef.current.target.set(0, 0, 0);
         }
+            
+        controlRef.current.update();
+      }
+
+      //Orthographic edge case handling
+      if (curCams[activeCamIdx].name === "Orthographic") {
+        nextCam.updateProjectionMatrix();
+      }
     }
   }, [activeCamIdx, curCams, set, activeSceneCam]);
 
   const curCamHandle = curCams[activeCamIdx].ref.current;
+
+  const { size } = useThree();
+  const halfWIDTH = size.width / 2;
+  const halfHEIGHT = size.height / 2;
+
 
   return (
     <>
@@ -395,17 +523,24 @@ function CamHandler({ cams }) {
         ref = {perspectiveCamRef}
         position = {cams[0].position}
         fov = {cams[0].fov}
-        near = {0.01}
+        near = {1}
       />
 
       <orthographicCamera
         ref = {orthoCamRef}
         position = {cams[1].position}
         zoom = {cams[1].zoom}
-        near = {0.01}
+        near = {1}
+        far = {1000}
+
+        top = {halfHEIGHT / cams[1].zoom}
+        bottom = {-halfHEIGHT / cams[1].zoom}
+        left = {-halfWIDTH / cams[1].zoom}
+        right = {halfWIDTH / cams[1].zoom}
       />
       
       <OrbitControls 
+        ref = {controlRef}
         camera = {curCamHandle}
         enabled = {curCamHandle === activeSceneCam}  
         enablePan = {true}
@@ -423,23 +558,29 @@ function Scene () {
     <>
 
       <ambientLight
-        intensity = {0.2}
+        intensity = {0.1}
+      />
+
+      <Environment 
+        preset = "night"
+        intensity = {0.2} 
+        background = {false} 
       />
 
       {/*KEY LIGHT. i.e. HIGH INTENSITY. Enhances shadows.*/} 
       <directionalLight 
-        position = {[50, 10, 50]} 
-        intensity = {10} 
+        position = {[150, 250, 150]} 
+        intensity = {3.5} 
         color = {"#FFFFFF"} 
         castShadow
 
         //Light Frustum
         shadow-camera-near = {1} 
-        shadow-camera-far = {200} 
-        shadow-camera-left = {-60}  
-        shadow-camera-right = {60} 
-        shadow-camera-top = {60} 
-        shadow-camera-bottom = {-60} 
+        shadow-camera-far = {300} 
+        shadow-camera-left = {-280}  
+        shadow-camera-right = {280} 
+        shadow-camera-top = {200} 
+        shadow-camera-bottom = {-200} 
 
         //ShadowMap res
         shadow-mapSize-width = {2048}
@@ -450,19 +591,22 @@ function Scene () {
 
       {/*FILL LIGHT. i.e. LOW INTENSITY. Softens.*/} 
       <directionalLight 
-        position = {[2, 4, 7]} 
-        intensity = {0} 
+        position = {[-50, 20, 50]} 
+        intensity = {0.8} 
         color = {"#ADD8E6"} 
       />
 
       {/*RIM LIGHT. Object specific. Edges enhancing.*/} 
       <pointLight 
-        position = {[0, 0, -2]} 
-        intensity = {0} 
+        position = {[-50, 50, -50]} 
+        intensity = {1.2} 
         color = {"#FFFDD0"} 
-        />
+      />
 
       <Ground />
+
+      <Sphere />
+      <Cube />
       <EnvObject />
       <MainObject />
       <PropObject />
@@ -484,7 +628,8 @@ export default function App () {
           type: THREE.PCFSoftShadowMap, 
         },
         
-        outputEncoding: THREE.LinearEncoding,
+        outputEncoding: THREE.sRGBEncoding,
+        dithering: true,
       }}
     >
       <Suspense fallback = {null}>
